6dd1458cc8576c9fb63441dfd87441c4c4707228
mono-nobe <ryoya.goemono@gmail.com>
2022-12-10 23:07:57 +0900

    参照機能の追加

--- 04.class/src/index.js
+++ 04.class/src/index.js
@@ -3,31 +3,64 @@
 const DB = require("./db");
 const read = require("./reader");
 const Minimist = require("minimist");
+const { prompt } = require("enquirer");
 
 const db = new DB();
 const argv = Minimist(process.argv.slice(2));
 
 if (!process.stdin.isTTY) {
-  addMemo();
+  createMemo();
 } else {
   if (argv.l) {
-    selectAll();
+    showAllMemos();
+  } else if (argv.r) {
+    showMemo();
   }
 }
 
-async function addMemo() {
+async function createMemo() {
   try {
     let lines = await read();
     await db.insert(lines.join("\n"));
-    console.log("### Memo added ###");
+    console.log("\nSaving is complete.");
   } finally {
     db.close;
   }
 }
 
-async function selectAll() {
+async function showAllMemos() {
   let rows = await db.selectAll();
   for (let row of rows) {
     console.log(row.text.split("\n")[0]);
   }
+}
+
+async function showMemo() {
+  let rows = await db.selectAll();
+  let firstLines = [];
+  for await (let row of rows) {
+    console.log(row.text.split("\n")[0]);
+    firstLines.push({
+      name: row.text.split("\n")[0],
+      value: row.id,
+    });
+  }
+
+  if (firstLines.length == 0) {
+    console.log("memo is empty.");
+    return;
+  }
+
+  const selectedFirstLine = await prompt({
+    type: "select",
+    name: "id",
+    message: "Choose a note you want to see:",
+    choices: firstLines,
+    result() {
+      return this.focused.value;
+    },
+  });
+
+  let selectedRow = await db.select(selectedFirstLine.id);
+  console.log("\n" + selectedRow.text);
 }
