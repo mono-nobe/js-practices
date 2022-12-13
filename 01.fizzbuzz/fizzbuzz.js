7e86db55b71f38b9502a0f2f7034a0a6059efd2b
mono-nobe <ryoya.goemono@gmail.com>
2022-12-13 17:21:27 +0900

    厳密等価演算子の採用

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 for (let num = 1; num <= 20; num++) {
-  if (num % 3 == 0 && num % 5 == 0) console.log("FizzBuzz");
-  else if (num % 3 == 0) console.log("Fizz");
-  else if (num % 5 == 0) console.log("Buzz");
+  if (num % 3 === 0 && num % 5 === 0) console.log("FizzBuzz");
+  else if (num % 3 === 0) console.log("Fizz");
+  else if (num % 5 === 0) console.log("Buzz");
   else console.log(num);
 }
